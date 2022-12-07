b613a322f5e21ec1445c9240c81c0df014bc5946
Corinne Burks <106447520+coburks@users.noreply.github.com>
2022-12-06 22:51:22 -0600

    Add files via upload

---
+++ SheFizzinOnMyBuzzAndItsMakingMeRange().py
@@ -0,0 +1,53 @@
+
+""""
+x = range(1,101)
+for n in x:
+    print(n)
+    for Fizz in x:
+        print("Fizz")
+        for Buzz in x:
+            print("Buzz")
+            for FizzBuzz in x:
+                print("FizzBuzz")
+
+
+
+
+
+
+
+
+
+
+Fizz = print("Fizz") for n in range(1, 101) if n % 3 == 0
+Buzz = n for n in range(1,101) if n % 5 == 0
+FizzBuzz = n for n in range(1, 101) if n % 3 == 0 and n % 5 == 0
+
+
+
+
+
+
+Fizz = n for n in range(1, 101) if n % 3 == 0
+Buzz = n for n in range(1,101) if n % 5 == 0
+FizzBuzz = n for n in range(1, 101) if n % 3 == 0 and n % 5 == 0
+
+if n fizz print "fizz"
+if n = Buzz
+"""
+
+
+
+
+for n in range(1, 101):
+    if (n % 3 == 0) and (n % 5 == 0) is True:
+        print("FizzBuzz")
+        continue
+    if (n % 3 == 0) is True:
+        print("Fizz")
+    elif (n % 5 == 0) is True:
+        print("Buzz")
+    else:
+        print(n)
+   
+        
